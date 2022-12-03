7c760abc6c770cccd2ec8053d03531bacf5eb93d
yoon <beatus96@naver.com>
2022-12-03 14:47:57 +0900

    docs : 기능목록 구현

---
+++ src/main/java/pairmatching/Loading.java
@@ -0,0 +1,11 @@
+package pairmatching;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loading {
+}
---
+++ src/main/java/pairmatching/PairMatchingProgram.java
@@ -0,0 +1,16 @@
+package pairmatching;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class PairMatchingProgram {
+
+    private List<String> backendCrew;
+    private List<String> frontendCrew;
+
+
+
+}
