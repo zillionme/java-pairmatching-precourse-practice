15d2afc5e0827eec1cc12b41b2568dfd46df8aa4
yoon <beatus96@naver.com>
2022-12-03 14:48:21 +0900

    feat : 파일 읽기 기능 추가

--- src/main/java/pairmatching/Loading.java
+++ src/main/java/pairmatching/Loading.java
@@ -8,4 +8,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Loading {
+    // 파일 내용 불러와서 리스트에 저장하기
+    String path = "src/main/resources/";
+    String fileName = "backend-crew.md";
+    File file = new File(path + fileName);
+
+
+    // FileNotFoundException 과 IOException 발생가능성
+    public List<String> load() throws IOException {
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> result = new ArrayList<>();
+        String line = "";
+
+        while ((line = bufferedReader.readLine()) != null) { // 파일 내 문자열을 1줄씩 읽기 while
+            //크루 이름 중복 불가 Set으로?
+            result.add(line);
+            }
+
+        return result;
+        }
+
 }
